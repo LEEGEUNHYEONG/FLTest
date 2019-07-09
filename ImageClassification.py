488aff2fbf6b9fd4e61f5d45c9da2136898e7c3e
Nanara <nanara1119@gmail.com>
2019-07-09 14:33:27 +0900

    1. ImageClassification 끝 2. TextGeneration 생성

--- ImageClassification.py
+++ ImageClassification.py
@@ -1,3 +1,6 @@
+'''
+https://www.tensorflow.org/federated/tutorials/federated_learning_for_image_classification
+'''
 # %%
 from __future__ import absolute_import, division, print_function
 
@@ -84,14 +87,167 @@ def create_compiled_keras_model():
     return model
 
 
-#%%
+# %%
 def model_fn():
     keras_model = create_compiled_keras_model()
     return tff.learning.from_compiled_keras_model(keras_model, sample_batch)
 
-#%%
+
+# %%
 iterative_process = tff.learning.build_federated_averaging_process(model_fn)
-#   todo : 더이상 진행 불가, tensorflow-federated 버전 업데이트 됬지만 pip에서 업데이트 안됨
+print(str(iterative_process.initialize.type_signature))
+
+# %%
+state = iterative_process.initialize()
+state, metrics = iterative_process.next(state, federated_train_data)
+print('round 1, metrics={}'.format(metrics))
+
+# %%
+for round_num in range(2, 11):
+    state, metrics = iterative_process.next(state, federated_train_data)
+    print('round {:2d} , metrics{}'.format(round_num, metrics))
+
+# %%
+MnistVariables = collections.namedtuple(
+    'MnistVariables', 'weights bias num_examples loss_sum accuracy_sum')
+
+
+def create_mnist_variables():
+    return MnistVariables(
+        weights=tf.Variable(
+            lambda: tf.zeros(dtype=tf.float32, shape=(784, 10)),
+            name='weights',
+            trainable=True),
+        bias=tf.Variable(
+            lambda: tf.zeros(dtype=tf.float32, shape=(10)),
+            name='bias',
+            trainable=True),
+        num_examples=tf.Variable(0.0, name='num_examples', trainable=False),
+        loss_sum=tf.Variable(0.0, name='loss_sum', trainable=False),
+        accuracy_sum=tf.Variable(0.0, name='accuracy_sum', trainable=False))
+
+
+def mnist_forward_pass(variables, batch):
+    y = tf.nn.softmax(tf.matmul(batch['x'], variables.weights) + variables.bias)
+    predictions = tf.cast(tf.argmax(y, 1), tf.int32)
+
+    flat_labels = tf.reshape(batch['y'], [-1])
+    loss = -tf.reduce_mean(tf.reduce_sum(
+        tf.one_hot(flat_labels, 10) * tf.log(y), reduction_indices=[1]))
+    accuracy = tf.reduce_mean(
+        tf.cast(tf.equal(predictions, flat_labels), tf.float32))
+
+    num_examples = tf.cast(tf.size(batch['y']), tf.float32)
+
+    variables.num_examples.assign_add(num_examples)
+    variables.loss_sum.assign_add(loss * num_examples)
+    variables.accuracy_sum.assign_add(accuracy * num_examples)
+
+    return loss, predictions
+
+
+def get_local_mnist_metrics(variables):
+    return collections.OrderedDict([
+        ('num_examples', variables.num_examples),
+        ('loss', variables.loss_sum / variables.num_examples),
+        ('accuracy', variables.accuracy_sum / variables.num_examples)
+    ])
+
+
+@tff.federated_computation
+def aggregate_mnist_metrics_across_clients(metrics):
+    return {
+        'num_examples': tff.federated_sum(metrics.num_examples),
+        'loss': tff.federated_mean(metrics.loss, metrics.num_examples),
+        'accuracy': tff.federated_mean(metrics.accuracy, metrics.num_examples)
+    }
+
+
+# %%
+class MnistModel(tff.learning.Model):
+
+    def __init__(self):
+        self._variables = create_mnist_variables()
+
+    @property
+    def trainable_variables(self):
+        return [self._variables.weights, self._variables.bias]
+
+    @property
+    def non_trainable_variables(self):
+        return []
+
+    @property
+    def local_variables(self):
+        return [
+            self._variables.num_examples, self._variables.loss_sum,
+            self._variables.accuracy_sum
+        ]
+
+    @property
+    def input_spec(self):
+        return collections.OrderedDict([('x', tf.TensorSpec([None, 784],
+                                                            tf.float32)),
+                                        ('y', tf.TensorSpec([None, 1], tf.int32))])
+
+    #   todo : autograph 설정 해줘야 에러 나지 않음
+    @tf.function(autograph=False)
+    def forward_pass(self, batch, training=True):
+        del training
+        loss, predictions = mnist_forward_pass(self._variables, batch)
+        return tff.learning.BatchOutput(loss=loss, predictions=predictions)
+
+    #   todo : autograph 설정 해줘야 에러 나지 않음
+    @tf.function(autograph=False)
+    def report_local_outputs(self):
+        return get_local_mnist_metrics(self._variables)
+
+    @property
+    def federated_output_computation(self):
+        return aggregate_mnist_metrics_across_clients
+
+
+# %%
+class MnistTrainableModel(MnistModel, tff.learning.TrainableModel):
+
+    @tf.function(autograph=False)
+    def train_on_batch(self, batch):
+        output = self.forward_pass(batch)
+        optimizer = tf.train.GradientDescentOptimizer(0.02)
+        optimizer.minimize(output.loss, var_list=self.trainable_variables)
+        return output
+
+
+# %%
+iterative_process = tff.learning.build_federated_averaging_process(MnistTrainableModel)
+state = iterative_process.initialize()
+
+#%%
+state, metrics = iterative_process.next(state, federated_train_data)
+print('round 1, metrics={}'.format(metrics))
+
+for round_num in range(2, 11) :
+    state, metrics = iterative_process.next(state, federated_train_data)
+    print('round {:2d}, metrics={}'.format(round_num, metrics))
+
+#%%
+'''
+    Evaluation
+'''
+evaluation = tff.learning.build_federated_evaluation(MnistModel)
+print(str(evaluation.type_signature))
+
+#%%
+train_metrics = evaluation(state.model, federated_train_data)
+print(str(train_metrics))
+
+#%%
+federated_test_data = make_federated_data(emnist_test, sample_clients)
+print(len(federated_test_data), federated_test_data[0])
+
+#%%
+test_metrics = evaluation(state.model, federated_test_data)
+print(test_metrics)
 
 
 
---
+++ TextGeneration.py
@@ -0,0 +1,6 @@
+'''
+https://www.tensorflow.org/federated/tutorials/federated_learning_for_text_generation
+'''
+
+#%%
+from __future__ import absolute_import, division, print_function
