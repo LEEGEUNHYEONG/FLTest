fa5f9798fcc008d28cd096a5093536d0049bcdff
Nanara <nanara1119@gmail.com>
2019-07-16 14:04:41 +0900

    1. CustomFederated 2 완료

--- CustomFederated2.py
+++ CustomFederated2.py
@@ -52,11 +52,12 @@ print(federate_train_data[5][-1]['y'])
 
 # %%
 from matplotlib import pyplot as plt
+
 plt.imshow(federate_train_data[5][-1]['x'][-1].reshape(28, 28), cmap='gray')
 plt.grid(False)
 plt.show()
 
-#%%
+# %%
 '''
     On combining TensorFlow and TensorFlowFederated
     Defining a loss functions 
@@ -67,32 +68,148 @@ BATCH_TYPE = tff.NamedTupleType([
 ])
 print(str(BATCH_TYPE))
 
-#%%
+# %%
 MODEL_TYPE = tff.NamedTupleType([
     ('weights', tff.TensorType(tf.float32, [784, 10])),
     ('bias', tff.TensorType(tf.float32, [10]))
 ])
 print(str(MODEL_TYPE))
 
-#%%
+
+# %%
 @tff.tf_computation(MODEL_TYPE, BATCH_TYPE)
 def batch_loss(model, batch):
     predicted_y = tf.nn.softmax(tf.matmul(batch.x, model.weights) + model.bias)
     return -tf.reduce_mean(tf.reduce_sum(tf.one_hot(batch.y, 10) * tf.log(predicted_y), reduction_indices=[1]))
 
+
 print(str(batch_loss.type_signature))
 
-#%%
+# %%
 initial_model = {
-    'weights' : np.zeros([784, 10], dtype=np.float32),
-    'bias' : np.zeros([10], dtype=np.float32)
+    'weights': np.zeros([784, 10], dtype=np.float32),
+    'bias': np.zeros([10], dtype=np.float32)
 }
 
 sample_batch = federate_train_data[5][-1]
 
 print(batch_loss(initial_model, sample_batch))
 
-#%%
+# %%
 '''
     Gradient descent on a single batch
 '''
+
+
+@tff.tf_computation(MODEL_TYPE, BATCH_TYPE, tf.float32)
+def batch_train(initial_model, batch, learning_rate):
+    model_vars = tff.utils.get_variables('v', MODEL_TYPE)
+    init_model = tff.utils.assign(model_vars, initial_model)
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    with tf.control_dependencies([init_model]):
+        train_model = optimizer.minimize(batch_loss(model_vars, batch))
+
+    with tf.control_dependencies([train_model]):
+        return tff.utils.identity(model_vars)
+
+
+print(str(batch_train.type_signature))
+
+# %%
+model = initial_model
+losses = []
+for _ in range(5):
+    model = batch_train(model, sample_batch, 0.1)
+    losses.append(batch_loss(model, sample_batch))
+
+print(losses)
+
+# %%
+'''
+    Gradient descent on a sequence of local data
+'''
+LOCAL_DATA_TYPE = tff.SequenceType(BATCH_TYPE)
+
+
+@tff.federated_computation(MODEL_TYPE, tf.float32, LOCAL_DATA_TYPE)
+def local_train(initial_model, learning_rate, all_batches):
+    @tff.federated_computation(MODEL_TYPE, BATCH_TYPE)
+    def batch_fn(model, batch):
+        return batch_train(model, batch, learning_rate)
+
+    return tff.sequence_reduce(all_batches, initial_model, batch_fn)
+
+
+print(str(local_train.type_signature))
+
+# %%
+locally_trained_model = local_train(initial_model, 0.1, federate_train_data[5])
+print(locally_trained_model)
+
+# %%
+'''
+    Local evaluation
+'''
+
+
+@tff.federated_computation(MODEL_TYPE, LOCAL_DATA_TYPE)
+def local_eval(model, all_batches):
+    return tff.sequence_sum(
+        tff.sequence_map(
+            tff.federated_computation(lambda b: batch_loss(model, b), BATCH_TYPE), all_batches))
+
+
+print(str(local_eval.type_signature))
+
+# %%
+print('initial_model_loss = ', local_eval(initial_model, federate_train_data[5]))
+print('locally_trained_model_loss = ', local_eval(locally_trained_model, federate_train_data[5]))
+
+# %%
+print('initial_model_loss = ', local_eval(initial_model, federate_train_data[0]))
+print('locally_trained_model_loss = ', local_eval(locally_trained_model, federate_train_data[0]))
+
+# %%
+'''
+    Federated evaluation
+'''
+SERVER_MODEL_TYPE = tff.FederatedType(MODEL_TYPE, tff.SERVER, all_equal=True)
+CLIENT_DATA_TYPE = tff.FederatedType(LOCAL_DATA_TYPE, tff.CLIENTS)
+
+
+@tff.federated_computation(SERVER_MODEL_TYPE, CLIENT_DATA_TYPE)
+def federate_eval(model, data):
+    return tff.federated_mean(tff.federated_map(local_eval, [tff.federated_broadcast(model), data]))
+
+
+print('initial_model loss = ', federate_eval(initial_model, federate_train_data))
+print('locally_traind_model loss = ', federate_eval(locally_trained_model, federate_train_data))
+
+# %%
+'''
+    Federated training
+'''
+SERVER_FLOAT_TYPE = tff.FederatedType(tf.float32, tff.SERVER, all_equal=True)
+
+
+@tff.federated_computation(SERVER_MODEL_TYPE, SERVER_FLOAT_TYPE, CLIENT_DATA_TYPE)
+def federated_train(model, learning_rate, data):
+    return tff.federated_mean(
+        tff.federated_map(
+            local_train, [tff.federated_broadcast(model), tff.federated_broadcast(learning_rate),
+                          data]))
+
+
+model = initial_model
+learning_rate = 0.1
+for round_num in range(5):
+    model = federated_train(model, learning_rate, federate_train_data)
+    learning_rate = learning_rate * 0.9
+    loss = federate_eval(model, federate_train_data)
+
+    print('round {}, loss={}'.format(round_num, loss))
+
+# %%
+print('initial_model loss = ', federate_eval(initial_model, federate_test_data))
+print('trained_model loss = ', federate_eval(model, federate_test_data))
