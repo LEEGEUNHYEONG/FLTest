53543abb093c9d884a71ce4736ec6655240e4b37
Nanara <nanara1119@gmail.com>
2019-08-30 17:32:28 +0900

    1. BaseFedClient 추가

--- Base/BaseFedClient.py
+++ Base/BaseFedClient.py
@@ -3,9 +3,9 @@ class BaseFedClient:
     def __init__(self):
         pass
 
-    def test(self):
-        pass
-
-
-def test2():
-    print("test2")
+    '''
+        1. data split ?
+        2. epoch, batch size 설정
+        3. fit
+        4. evaluate
+    '''
