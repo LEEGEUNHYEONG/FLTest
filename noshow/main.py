98f65c7edcefbfb157d7b46a2ecb7e58ee15379f
Nanara <nanara1119@gmail.com>
2019-09-20 15:56:24 +0900

    1. balanced 데이터 테스트 진행

--- noshow/main.py
+++ noshow/main.py
@@ -1,26 +1,23 @@
 # %%
 '''
 https://www.kaggle.com/joniarroba/noshowappointments
+
+!!!
+https://www.kaggle.com/belagoesr/predicting-no-show-downsampling-approach-with-rf
 '''
+
 # %%
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from sklearn.utils.multiclass import unique_labels
-from sklearn.metrics import accuracy_score
-
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy import stats
-
-import scipy as sp
-from sklearn.model_selection import RandomizedSearchCV
-from functools import partial
+import pandas as pd
+import tensorflow as tf
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+import Base.BaseServer as BaseServer
 
 le = LabelEncoder()
+server = BaseServer.BaseServer.get_instance()
 
 # %%
 df_train = pd.read_csv('noshow/KaggleV2-May-2016.csv')
@@ -46,12 +43,12 @@ df_train['No-show'] = df_train['No-show'].map({"No": 0, "Yes": 1})
 less_than_100 = ['MORADA DE CAMBURI', 'PONTAL DE CAMBURI', 'ILHA DO BOI', 'ILHA DO FRADE',
                  'AEROPORTO', 'ILHAS OCEÂNICAS DE TRINDADE', 'PARQUE INDUSTRIAL']
 
-df_train.loc[df_train.Neighbourhood.isin(less_than_100), 'Neighbourhood'] = "OTHERS"
+#df_train.loc[df_train.Neighbourhood.isin(less_than_100), 'Neighbourhood'] = "OTHERS"
 
-df_train.drop(['PatientId', 'AppointmentID', 'ScheduledDay', 'AppointmentDay'], axis=1, inplace=True)
+df_train.drop(['PatientId', 'AppointmentID', 'ScheduledDay', 'AppointmentDay', 'Neighbourhood'], axis=1, inplace=True)
 
-le.fit(df_train['Neighbourhood'].drop_duplicates())
-df_train['Neighbourhood'] = le.transform(df_train['Neighbourhood'])
+#le.fit(df_train['Neighbourhood'].drop_duplicates())
+#df_train['Neighbourhood'] = le.transform(df_train['Neighbourhood'])
 
 # %%
 y_train = df_train['No-show']
@@ -62,27 +59,29 @@ train_stats = train_stats.transpose()
 
 X_train = (X_train - train_stats['mean'] ) / train_stats['std']
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=.2)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15, shuffle=True)
 
 result = [x for x in y_train if x == 1]
-print(len(y_train), " : " , len(result))
+print("total train size : ", len(y_train), " / no :" , (len(y_train) - len(result)), " : yes " ,len(result))
 
 result = [x for x in y_val if x == 1]
-print(len(y_val), " : ", len(result))
+print("total test size : ", len(y_val), " / no :" ,  (len(y_val) - len(result)), " : yes " ,len(result))
+
+# %%
+def print_cm(y_val, y_pred):
+    cm = confusion_matrix(y_val, y_pred)
+    print(cm)
+    print("accuracy : {}".format(((cm[0][0] + cm[1][1]) / len(X_val)) * 100))
 
 # %%
 def build_model():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units=64, activation=tf.nn.relu, input_dim=10),
-        #tf.keras.layers.Dropout(0.25),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
-        #tf.keras.layers.Dropout(0.25),
-        #tf.keras.layers.Dense(units=1, kernel_initializer='uniform', activation='sigmoid')
-        tf.keras.layers.Dense(1)
-    ])
 
-    model.compile(optimizer=tf.keras.optimizers.SGD(momentum=0.9, learning_rate=0.1),
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(units=64, activation=tf.nn.relu, input_dim=9),
+        tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
                   loss=tf.keras.losses.binary_crossentropy,
                   metrics=['accuracy'])
 
@@ -91,21 +90,14 @@ def build_model():
 
 model = build_model()
 # %%
-model.fit(X_train, y_train, batch_size=64, epochs=5)
+model.fit(X_train, y_train, batch_size=32, epochs=5)
 
-# %%
+# %%    nn
 y_pred = model.predict(X_val)
-
-result = [x for x in y_pred if x > 0.5]
-print(len(result))
-
 y_pred = (y_pred > 0.5)
 
 print("result count : ", np.count_nonzero(y_pred == 0), np.count_nonzero(y_pred == 1))
-
-cm = confusion_matrix(y_val, y_pred)
-print(cm)
-print("accuracy : {}".format(((cm[0][0] + cm[1][1]) / len(X_val)) * 100))
+print_cm(y_val, y_pred)
 
 # %%    random forest
 from sklearn.ensemble import RandomForestClassifier
@@ -113,10 +105,9 @@ from sklearn.ensemble import RandomForestClassifier
 forest = RandomForestClassifier(n_estimators=100)
 forest.fit(X_train, y_train)
 y_pred = forest.predict(X_val)
+print("result count : ", np.count_nonzero(y_pred == 0), np.count_nonzero(y_pred == 1))
+print_cm(y_val, y_pred)
 
-cm = confusion_matrix(y_val, y_pred)
-print(cm)
-print("accuracy : {}".format(((cm[0][0] + cm[1][1]) / len(X_val)) * 100))
 
 # %% GradientBoosting
 from sklearn.ensemble import GradientBoostingClassifier
@@ -125,10 +116,8 @@ gbc = GradientBoostingClassifier(random_state=0)
 gbc.fit(X_train, y_train)
 
 y_pred = gbc.predict(X_val)
-
-cm = confusion_matrix(y_val, y_pred)
-print(cm)
-print("accuracy : {}".format(((cm[0][0] + cm[1][1]) / len(X_val)) * 100))
+print("result count : ", np.count_nonzero(y_pred == 0), np.count_nonzero(y_pred == 1))
+print_cm(y_val, y_pred)
 
 # %% Support Vector
 from sklearn import svm
@@ -136,7 +125,70 @@ clf = svm.SVC(gamma=0.001, C = 1)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_val)
+print("result count : ", np.count_nonzero(y_pred == 0), np.count_nonzero(y_pred == 1))
+print_cm(y_val, y_pred)
 
-cm = confusion_matrix(y_val, y_pred)
-print(cm)
-print("accuracy : {}".format(((cm[0][0] + cm[1][1]) / len(X_val)) * 100))
+# %%
+def run_federate(user_number = 3, round_number =2, epoch = 20, batch_size = 10):
+    train_list = np.array_split(X_train, user_number+1)
+    test_list = np.array_split(y_train, user_number+1)
+    print("federate start : user number : {}, total size : {}, each size : {}".format(user_number, len(X_train),
+                                                                                      len(train_list[0])))
+    local_model = build_model()
+    for r in range(round_number):
+        local_weight_list = []
+        for user in range(user_number):
+            print('---------')
+            print("round : ", (r + 1), " / user : ", (user+1))
+            print('---------')
+            server_weight = server.get_weight()
+            if server_weight is not None:
+                local_model.set_weights(server_weight)
+            local_model.fit(train_list[user], test_list[user], epochs=epoch, batch_size=batch_size, verbose=0)
+            local_weight_list.append(local_model.get_weights())
+        server.update_weight(local_weight_list)
+    print("federate end")
+
+    predict_federate(train_list[-1], test_list[-1], X_val, y_val)
+    non_federate(train_list[-1], test_list[-1], X_val, y_val)
+
+
+# %%
+def non_federate(x_train, y_train, x_test, y_test ):
+    print("non_federate validation start")
+    model = build_model()
+    model.fit(x_train, y_train, epochs=5, verbose=0)
+    result = model.predict(x_test)
+    result = (result > 0.5)
+    print_cm(y_test, result)
+    print("non_federate validation end\n")
+
+
+
+# %%
+def predict_federate(x_train, y_train, x_test, y_test):
+    print("federate validation start")
+    model = build_model()
+    model.set_weights(server.get_weight())
+    model.fit(x_train, y_train, epochs=5, verbose=0)
+    result = model.predict(x_test)
+    result = (result > 0.5)
+    print_cm(y_test, result)
+    print("federate validation end")
+
+# %%
+run_federate(user_number=10, round_number=100, batch_size=20, epoch=5)
+
+# %%
+def test():
+    model = build_model()
+    model.set_weights(server.get_weight())
+    model.fit(X_train, y_train, epochs=5)
+    result = model.predict(X_val)
+    result = (result > 0.5)
+    print_cm(y_val, result)
+
+test()
+
+#%%
+server.init_weight()
